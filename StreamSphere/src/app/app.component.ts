6618312d54d58164ffc2dd36ad720cb7dfb74549
Oskar Wichtowski <oskar.wichtowski3@gmail.com>
2024-06-16 21:30:57 +0200

    create seperate navbar component and fix app.comp,ts

--- StreamSphere/src/app/app.component.ts
+++ StreamSphere/src/app/app.component.ts
@@ -1,26 +1,29 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+// src/app/app.component.ts
+import { CommonModule, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
-
+import { SessionService } from './session.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink],
+  imports: [
+    RouterOutlet, 
+    CommonModule, 
+    RouterLink,
+    NgIf
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'StreamSphere';
   sessionState: boolean = false;
- 
+
+  constructor(private sessionService: SessionService) {}
 
   ngOnInit() {
-    const sessionState = this.readLocalStorageValue('sessionState');
-    this.sessionState = sessionState === 'true' ? true : false;
-  }
-  readLocalStorageValue(key: string): string | null {
-    return localStorage.getItem(key);
+
   }
 }
