6f65c8911826b1162debf87fb199374fba8e9cb5
Oskar Wichtowski <oskar.wichtowski3@gmail.com>
2024-06-18 21:41:42 +0200

    chore: Update npm install command to use --legacy-peer-deps flag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@
 import { CommonModule, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RouterLink, Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink, NgIf, Router],
+  imports: [RouterOutlet, CommonModule, RouterLink, NgIf],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
