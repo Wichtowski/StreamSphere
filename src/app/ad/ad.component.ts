0c2907268579e94c132ffe20478f2e042aa3f0dc
Oskar Wichtowski <oskar.wichtowski3@gmail.com>
2024-06-21 00:10:41 +0200

    new logic to ad component and some changes to landing page

--- src/app/ad/ad.component.ts
+++ src/app/ad/ad.component.ts
@@ -1,24 +1,28 @@
 import { Component } from '@angular/core';
-import { CommonModule, NgSwitch } from '@angular/common';
+import { CommonModule, NgIf, NgSwitch } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { SessionService } from '../services/session.service';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-ad',
   standalone: true,
-  imports: [CommonModule, RouterLink, NgSwitch],
+  imports: [CommonModule, RouterLink, NgSwitch, NgIf],
   templateUrl: './ad.component.html',
   styleUrl: './ad.component.scss',
 })
 export class AdComponent {
-  subscriptionType: string = 'free' || 'premium';
+  subscriptionType: string = '';
+  randAd: number = 0; 
+  // Math.floor(Math.random() * 3);
 
-  constructor(private sessionService: SessionService) {}
+  constructor(private localStorageService: LocalStorageService) {}
 
   ngOnInit(): void {
-    const subscriptionType = this.sessionService.readSessionStorageValue('subscriptionType');
+    const subscriptionType = this.localStorageService.getFieldFromUserLocalStorage('subscriptionType');
     if (subscriptionType) {
       this.subscriptionType = subscriptionType;
+      console.log(this.randAd);
+      console.log(this.subscriptionType);
     }
   }
 }
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
